4c26715cfa4ef588da6226623f0cf2ead23e8b5d
GarrettMcNaLang <112025126+GarrettMcNaLang@users.noreply.github.com>
2024-11-05 14:06:21 -0500

    BasicsForSpawnPlayerTiles
    StartedWorkOnLevelCompleteTiles

--- Assets/PlayerSpawnSpace.cs
+++ Assets/PlayerSpawnSpace.cs
@@ -2,5 +2,13 @@ using UnityEngine;
 
 public class PlayerSpawnSpace : MonoBehaviour
 {
-   
+    public GameObject playerPrefab;
+
+    public GameObject spawnPoint;
+
+    private void OnEnable()
+    {
+        
+        Instantiate(playerPrefab, spawnPoint.transform.position, Quaternion.identity);
+    }
 }
